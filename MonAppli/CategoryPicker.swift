4b7352139744f134f66cc5251b1b6ac7cde27423
Thomas Anquetin <@thomanq>
2025-04-30 20:47:17 +0200

    Fix navigation

--- MonAppli/CategoryPicker.swift
+++ MonAppli/CategoryPicker.swift
@@ -1,39 +1,49 @@
 import SwiftUI
 
 struct CategoryPicker: View {
-    @Environment(\.presentationMode) var presentationMode
-    let selectedDate: Date
-    @State private var categories: [Category] = []
-    @State private var exerciseTypes: [ExerciseType] = []
-    @State private var showingAddSheet = false
-    
-    init(selectedDate: Date) {
-        self.selectedDate = selectedDate
-    }
-    
-    var body: some View {
-        NavigationStack {
-            List(categories) { category in
-                NavigationLink(destination: ExerciseTypePickerView(
-                    category: category,
-                    categoryPickerPresentationMode: presentationMode
-                )) {
-                    Text(category.name)
-                        .font(.headline)
-                }
-            }
-            .navigationTitle("Select a category")
-            .toolbar {
-                Button(action: { showingAddSheet = true }) {
-                    Image(systemName: "plus")
-                }
-            }
-            .onAppear {
-                categories = DatabaseHelper.shared.fetchCategories()
-            }
-            .sheet(isPresented: $showingAddSheet) {
-                AddExerciseTypeSheet(isPresented: $showingAddSheet)
-            }
+  @Binding var showCategoryPicker: Bool
+
+  let selectedDate: Date
+  @State private var categories: [Category] = []
+  @State private var exerciseTypes: [ExerciseType] = []
+  @State private var showingAddSheet = false
+
+  init(selectedDate: Date, showCategoryPicker: Binding<Bool>) {
+    self.selectedDate = selectedDate
+    _showCategoryPicker = showCategoryPicker
+  }
+
+  var body: some View {
+    VStack {
+      List(categories) { category in
+        NavigationLink(
+          destination: ExerciseTypePickerView(
+            category: category,
+            showCategoryPicker: $showCategoryPicker
+          )
+        ) {
+          Text(category.name)
+            .font(.headline)
         }
+      }
+      .toolbar {
+        ToolbarItem(placement: .principal) {
+          Text("Select a category")
+            .font(.headline)
+        }
+        ToolbarItem(placement: .topBarTrailing) {
+          Button(action: { showingAddSheet = true }) {
+            Image(systemName: "plus")
+          }
+        }
+      }
+      .navigationBarTitleDisplayMode(.inline)
+      .onAppear {
+        categories = DatabaseHelper.shared.fetchCategories()
+      }
+      .sheet(isPresented: $showingAddSheet) {
+        AddExerciseTypeSheet(isPresented: $showingAddSheet)
+      }
     }
+  }
 }
